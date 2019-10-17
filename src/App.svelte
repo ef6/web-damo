cd7d06fe70d825115a5d14cb32bf21964fe643f9
evan <evan7fan@outook.com>
2019-10-17 11:50:28 +0800

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,31 @@
 <script>
 	let characters = ['❤','💛','💚','💙','💜']; // 特点
-	let density = 80 // 密度
+	let density = 60 // 密度
 	let confetti = new Array(density).fill()  // 五彩纸屑
 		.map((_, i) => {
 			return {
 				character: characters[i % characters.length],
 				x: Math.random() * 100,
-				y: -5 - Math.random() * 100,
+				y: -10 - Math.random() * 100,
 				r: 0.2 + Math.random() * 1
 			};
 		})
-		.sort((a, b) => a.r - b.r);
+		.sort((a, b) => a.r - b.r); // 使渲染时从小到大渲染，大的覆盖小的。
 
 	function loop() {
 		confetti = confetti.map(emoji => {
-			emoji.y += 0.7 * emoji.r;
-			if (emoji.y > 100) emoji.y = -5;
+			emoji.y += 0.5 * emoji.r; // 调整下落的速度
+			if (emoji.y > 100) emoji.y = -10;
 			return emoji;
 		});
 		requestAnimationFrame(loop);
 	}
 
-	loop();
+	requestAnimationFrame(loop);
 </script>
 
 <style>
-	:global(body) {	overflow: hidden;}
+	:global(body) { overflow: hidden;}
 	span {
 		position: absolute;
 		font-size: 5vw;
